f1987fdbd8297bd094e9d0b234233abe390e2122
Abhishek kumar <selfish.abhishek2602@gmail.com>
2026-06-07 15:42:43 +0530

    cors fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,12 +8,19 @@ const cors = require("cors")
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://your-vercel-app.vercel.app"
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            "http://localhost:5173",
+            "https://interview-ai-three-dusky.vercel.app"
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
 }));
 
 const authrouter = require("./routes/auth.route");
