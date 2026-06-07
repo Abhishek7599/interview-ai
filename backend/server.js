7c8ca519a514ccee66c1890eff545b681f0cebe0
Abhishek kumar <selfish.abhishek2602@gmail.com>
2026-06-07 14:57:11 +0530

    Add deployment marker

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
+
 const app = require("./src/app");
 const connecttodb = require("./src/config/database");
+
+const port = process.env.PORT || 3000;
+
 connecttodb();
 
-
-
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
