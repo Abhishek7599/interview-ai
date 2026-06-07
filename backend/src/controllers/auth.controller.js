07c0c3ce3280fae044e7bc2a0cdd466aba1abc40
Abhishek kumar <selfish.abhishek2602@gmail.com>
2026-06-07 14:23:10 +0530

    Fix production cookie settings

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -80,7 +80,12 @@ async function loginUserController(req,res) {
 
     )
 
-    res.cookie("token",token);
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+});
     res.status(200).json({
         message: "User LoggedIn Successfully",
         user:{
